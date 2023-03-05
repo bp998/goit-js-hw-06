c49803cbee8a5391a0394c44e40c2b145f1b5e15
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-03-05 18:38:23 +0100

    Task 1 and 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const log = (log) => console.log(log);
+
+[...ingredients].forEach((elem) => {
+  const item = document.createElement("li");
+  item.textContent = elem;
+  item.classList.add("item");
+  list.append(item);
+});
