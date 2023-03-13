38ea0fd23926862bbe95be7b3b708212d3895cd6
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-03-13 14:21:13 +0100

    setup

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const range = document.querySelector("#font-size-control");
-
-const text = document.querySelector("#text");
-
-range.addEventListener("input", (e) => {
-  text.style.fontSize = e.target.value + "px";
-});
